5f32dbb45723e3cc065c999901e5e4d1743ff3ba
Chimera <j.calderon3@gmail.com>
2021-01-19 19:09:45 -0500

    server script choices updated

--- server.js
+++ server.js
@@ -1,38 +1,81 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const Choice = require('inquirer/lib/objects/choice');
 
 const connection = mysql.createConnection({
-     host: 'localhost',
-     port: 3306,
-     user: 'root',
-     password: 'root',
-     database: 'employee_DB'
- });
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'employee_DB'
+});
 
- connection.connect(err => {
-     if (err) throw err;
-     console.log('connected as id' + connection.threadId);
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Welcome to the employee management system.');
+    mainMenu();
 
-//     connection.query("SELECT * FROM products", (err, res) => {
-//             if (err) throw err;
-//             console.log(res);
-//     })
+    function mainMenu() {
+        inquirer.prompt({
+                name: "choice",
+                type: "list",
+                message: "Please select an action from the list:"
+                choices: ["Add department, role, or employee", "View department, role or employee", "Update employee role"]
+            })
 
-//     connection.end();
-// });
+            .then(function(answer) {
+                switch (answer.choice) {
+                    case "Add department, role, or employee":
+                        addChoice();
+                        break;
 
+                    case "View department, role or employee":
+                        viewChoice();
+                        break;
 
-// function createProduct () {
-//     console.log("Inserting a new product...\n");
-//     const query = connection.query(
-//         "INSERT INTO products SET ?",
-//         {
-//             flavor: "Rocky Road",
-//             price: 3.0,
-//             quantity: 50,
-//         },
-//         (err, res) => {
-//             console.log(`${res.affectedRows} product inserted!\n`);
-//         }
-//     )
-// };
+                    case "Update employee role":
+                        updateChoice();
+                        break;
+                }
+            });
+
+    function addChoice() {
+        inquirer.prompt({
+            name: "addition",
+            type: "list",
+            message: "Please select an action from the list:"
+            choices: ["Add department", "Add role", "Add employee"]
+        })
+
+        .then(function(answer) {
+            switch (answer.addition) {
+                case "Add department":
+                //    addDept();
+            }
+        })
+    }
+
+    function viewChoice() {
+
+    }
+
+function returnMenu() {
+    inquirer.prompt({
+        name: "return-menu",
+        type: "list",
+        message: "Return to main menu?"
+        choices: ["Yes", "No"]
+    })
+
+    .then(function(answer) {
+        switch (answer['return-menu']) {
+            case "Yes":
+            mainMenu();
+            break;
+
+            case "No":
+            console.log("Thank you for using the employee management system");
+            connection.end();
+        }
+    }
+};
